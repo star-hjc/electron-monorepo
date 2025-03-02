ee401bc3ad33a454e16c5fb745d39a221a16e8bd
star_hjc <89001850+star-hjc@users.noreply.github.com>
2025-03-02 16:09:56 +0800

    📦 build: 更新架构

---
+++ electron/app/main.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from 'electron'
+
+
+
+app.whenReady().then(async () => {
+    createMainWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
+    })
+})
+
+
+function createMainWindow() {
+    const win = new BrowserWindow({
+        /** 隐藏菜单 */
+        width: 850,
+        height: 830,
+        autoHideMenuBar: true,
+        webPreferences: {
+            // preload: path.join(app.getAppPath(), '/preload/index.js'),
+            nodeIntegration: true,
+        },
+        title: 'xxx',
+    })
+
+    win.loadURL(`http://localhost:5173/`)
+
+    win.on('close', async () => {
+        console.log(win.id)
+    })
+    return win
+}
