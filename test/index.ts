d4e816202de6b865631b2c37b9e20c09b400e9ac
AllIsWell <admin@haokur.com>
2025-03-04 00:34:13 +0800

    ✨ feat: 添加rust调用dylib库

--- test/index.ts
+++ test/index.ts
@@ -1,4 +1,4 @@
 import NeonBridge from '../neon-bridge/index'
 
 console.log(NeonBridge,"index.ts::2行");
-console.log(NeonBridge.hello(),"index.ts::5行");
+console.log(NeonBridge.hello(),NeonBridge.run_dylib_func(),"index.ts::5行");
