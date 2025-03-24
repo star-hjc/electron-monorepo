38165fe3c728d6f8c933fa3a141fbac585bc9b1f
star_hjc <89001850+star-hjc@users.noreply.github.com>
2025-03-24 23:23:22 +0800

    👌 perf: 优化打包速度

--- electron/main/tsup.config.ts
+++ electron/main/tsup.config.ts
@@ -3,7 +3,7 @@ import path from 'node:path'
 import * as dotenv from 'dotenv'
 import dayjs from 'dayjs'
 import { defineConfig } from 'tsup'
-import { existsSync, readFileSync, writeFileSync } from 'node:fs'
+import { existsSync, readFileSync, writeFileSync, readdirSync } from 'node:fs'
 import { createRequire, builtinModules } from 'node:module'
 import { type ChildProcess, spawn, execSync } from 'node:child_process'
 import fs from 'fs-extra'
@@ -30,6 +30,7 @@ export default defineConfig(({ env, watch }) => {
 		tsconfig: 'tsconfig.json',
 		external: ['electron', /^@package\/.+/, /^electron\/.+/, ...builtinModules.flatMap(m => [m, `node:${m}`])],
 		loader: {
+			'.npmrc': 'copy',
 			'.icns': 'copy',
 			'.png': 'copy',
 			'.jpg': 'copy',
@@ -40,10 +41,19 @@ export default defineConfig(({ env, watch }) => {
 		plugins: [
 			{
 				name: 'electron-plugin',
-				buildStart: async() => {
-					fs.removeSync(outDir)
+				buildStart: () => {
+					if (existsSync(outDir)) {
+						for (const file of readdirSync(outDir)) {
+							const filePath = path.join(outDir, file)
+							const stats = fs.statSync(filePath)
+							if (stats.isDirectory() && file === 'node_modules') {
+								continue
+							}
+							fs.removeSync(filePath)
+						}
+					}
 				},
-				buildEnd: async() => {
+				buildEnd: () => {
 					if (ps) {
 						ps.removeAllListeners()
 						ps.kill()
@@ -76,11 +86,12 @@ export default defineConfig(({ env, watch }) => {
 			electronBuildConfig.directories.output = outputAppPath
 			electronBuildConfig.artifactName = `\${productName}-\${version}-\${arch}-${dayjs().format('YYMMDDHHmmss')}.\${ext}`
 			fs.removeSync(path.join(outputAppPath, 'win-unpacked'))
-			const packageJson = { main: buildMainPath, name, version, dependencies, devDependencies, scripts: { build: 'electron-builder --config=electron.config.json' }}
+			const packageJson = { main: buildMainPath, name, version, dependencies, devDependencies }
 			writeFileSync(`${outDir}/package.json`, JSON.stringify(packageJson, null, 2))
 			writeFileSync(`${outDir}/electron.config.json`, JSON.stringify(electronBuildConfig, null, 2))
 			const cwd = path.join(__dirname, outDir)
 			fs.copySync(path.join(envDir, 'electron/renderer/dist'), path.join(__dirname, '/dist/renderer'))
+			fs.copyFileSync(path.join(__dirname, '.npmrc'), path.join(__dirname, '/dist/.npmrc'))
 			execSync('npm install', { cwd, stdio: 'inherit' })
 			execSync('electron-builder --config=electron.config.json', { cwd, stdio: 'inherit' })
 		}
