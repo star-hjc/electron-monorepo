6f32db18b17309a61e8f736acb91c2046e2ce2a7
star_hjc <89001850+star-hjc@users.noreply.github.com>
2025-04-09 23:37:14 +0800

    🐛 fix: 修复创建日志时文件名称错误问题

--- electron/main/src/windows/index.ts
+++ electron/main/src/windows/index.ts
@@ -1,7 +1,7 @@
 import { createWindow as createMainWindow } from '@windows/main'
 import { createWindow as createMiniWindow } from '@windows/mini'
 import { logger } from 'modules/logger'
-import { app } from 'electron'
+import { app, BrowserWindow } from 'electron'
 
 async function initApplicationBefore() {
 	await initRendererLog()
@@ -17,13 +17,25 @@ async function initApplicationAfter() {
 }
 
 async function initRendererLog() {
-	app.on('browser-window-created', (event, win) => {
-		const title = win.title
+	app.on('browser-window-created', async(event, win) => {
+		const title = await getTitle(win)
 		win.webContents.on('console-message', (event, level, message, line, sourceId) => {
 			const log = logger(`renderer-${title}`)
 			message = message.replace(/^\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2}\.\d{3}\s+\(.+\)\s+/, '')
 			log[['debug', 'info', 'warn', 'error'][level]](`${message} (${sourceId}:${line} ${win.id})`)
 		})
+	})
+}
+
+async function getTitle(win:BrowserWindow) {
+	return new Promise(resolve => {
+		const timeId = setTimeout(callback, 1000)
+		win.webContents.once('did-start-loading', callback)
+		function callback() {
+			resolve(win.title)
+			win.webContents.off('did-start-loading', callback)
+			clearTimeout(timeId)
+		}
 	})
 }
 
