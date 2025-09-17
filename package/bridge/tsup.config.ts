c57cf0d34faa86b2e18082b5f132a0f692d865ea
star_hjc <89001850+star-hjc@users.noreply.github.com>
2025-09-17 12:05:59 +0800

    🔧 ci: rust addons 打包为app 不生成类型提示文件

--- package/bridge/tsup.config.ts
+++ package/bridge/tsup.config.ts
@@ -1,22 +1,24 @@
 import { defineConfig } from 'tsup'
 import path from 'path'
-// import fs from 'fs'
 
-export default defineConfig({
-	clean: true,
-	entryPoints: ['./index.ts'],
-	format: ['esm', 'cjs'],
-	dts: true,
-	outDir: 'dist',
-	esbuildPlugins: [
-		{
-			name: 'node-native-module',
-			setup(build) {
-				build.onResolve({ filter: /\.node$/ }, (args) => ({
-					path: path.join(args.resolveDir, args.path),
-					external: true // 标记为外部依赖
-				}))
+export default defineConfig(({ env }) => {
+	const NODE_ENV = env?.NODE_ENV || 'development'
+	return {
+		clean: true,
+		entryPoints: ['./index.ts'],
+		format: ['esm', 'cjs'],
+		dts: NODE_ENV !== 'production',
+		outDir: 'dist',
+		esbuildPlugins: [
+			{
+				name: 'node-native-module',
+				setup(build) {
+					build.onResolve({ filter: /\.node$/ }, (args) => ({
+						path: path.join(args.resolveDir, args.path),
+						external: true // 标记为外部依赖
+					}))
+				}
 			}
-		}
-	]
+		]
+	}
 })
